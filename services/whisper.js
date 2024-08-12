cbc45a4ee3a261879f8c23d78f6c338c742c6cc2
Bryan Vera <bveracachay@gmail.com>
2024-08-12 12:06:45 -0500

    se agrega el borrado de audio luego de ser procesado

--- services/whisper.js
+++ services/whisper.js
@@ -1,42 +1,47 @@
-require("dotenv").config();
-const fs = require("fs");
-const { Configuration, OpenAIApi } = require("openai");
-const axios = require("axios");
+require('dotenv').config();
+const fs = require('fs');
+const { Configuration, OpenAIApi } = require('openai');
+const axios = require('axios');
 
 /**
  *
  * @param {*} path url mp3
  */
 const voiceToText = async (path) => {
-  if (!fs.existsSync(path)) {
-    throw new Error("No se encuentra el archivo");
-  }
+	if (!fs.existsSync(path)) {
+		throw new Error('No se encuentra el archivo');
+	}
 
-  // mp3 file
-  const file = fs.createReadStream(path);
+	// mp3 file
+	const file = fs.createReadStream(path);
 
-  console.log("file", file.path);
+	console.log('file', file.path);
 
-  try {
-    const response = await axios.post(
-      "https://api.openai.com/v1/audio/transcriptions",
-      {
-        file,
-        model: "whisper-1",
-      },
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: `Bearer ${process.env.PROBANDO}`,
-        },
-      }
-    );
+	try {
+		const response = await axios.post(
+			'https://api.openai.com/v1/audio/transcriptions',
+			{
+				file,
+				model: 'whisper-1',
+			},
+			{
+				headers: {
+					'Content-Type': 'multipart/form-data',
+					Authorization: `Bearer ${process.env.PROBANDO}`,
+				},
+			}
+		);
 
-    return response.data.text;
-  } catch (err) {
-    console.log(err);
-    return "ERROR";
-  }
+		//delete all files in folder tmp
+		fs.readdirSync('./tmp').forEach((file) => {
+			fs.unlinkSync(`./tmp/${file}`);
+		});
+
+		return response.data.text;
+	} catch (err) {
+		console.log(err);
+		return 'ERROR';
+	}
 };
 
 module.exports = { voiceToText };
