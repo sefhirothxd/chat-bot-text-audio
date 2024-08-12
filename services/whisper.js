e4e2268bbdd54fd678dbcbba306d507b7d3dc2e8
Bryan Vera <bveracachay@gmail.com>
2024-08-12 13:00:18 -0500

    fix error temp

--- services/whisper.js
+++ services/whisper.js
@@ -32,27 +32,6 @@ const voiceToText = async (path) => {
 			}
 		);
 
-		//delete all files in folder tmp
-		try {
-			// Verifica si el directorio existe antes de intentar leerlo
-			if (fs.existsSync('./tmp')) {
-				// Lee el contenido del directorio y elimina cada archivo
-				fs.readdirSync('./tmp').forEach((file) => {
-					try {
-						fs.unlinkSync(`./tmp/${file}`);
-					} catch (err) {
-						console.error(`Error al eliminar el archivo: ${file}`, err);
-					}
-				});
-			} else {
-				console.log(
-					'El directorio ./tmp no existe, continuando con el proceso.'
-				);
-			}
-		} catch (err) {
-			console.error('Error al leer el directorio ./tmp:', err);
-		}
-
 		return response.data.text;
 	} catch (err) {
 		console.log(err);
